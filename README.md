6f784f302d8dd090b77230d046b122b13fe2c036
Pree254 <pripetym.pm@gmail.com>
2025-04-22 21:10:35 +0300

    square

---
+++ README.md
@@ -0,0 +1,1 @@
+1 Thisrepository contains our code for data breed project did in class session.
---
+++ square.py
@@ -0,0 +1,9 @@
+i #!/usr/bin/python3
+2
+3 def square(num):
+    """function returns square of a number"""
+    result-num * num
+
+    print(result) 
+
+    square(5)
